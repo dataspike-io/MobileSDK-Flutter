0982a94358e726ed2aba17413fb2eaeeba9041c2
Mikhail Tiranov <mikhail.tiranov@mozzartbet.com>
2025-09-03 15:24:22 +0200

    remove old images

--- example/lib/src/dependencies_provider/model/sample_app_dependencies.dart
+++ example/lib/src/dependencies_provider/model/sample_app_dependencies.dart
@@ -11,7 +11,7 @@ class SampleAppDependencies {
 
   static const dependencies = SampleAppDependencies(
     isDebug: true,
-    dsApiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJvMzU0ZDUyZjlmZDQzNDMyIiwidHBlIjoxLCJhcCI6bnVsbCwicyI6IjFmMDc2MWZlLTJiMmEtNmY0My05ZGNlLTI5MDc2ZTA1NjkwOSIsImlzcyI6ImRhdGFzcGlrZS5pbyJ9.cNjCgQf5WhAZJWHwFBTCFHD3gUE0tsIRG1mGWOe7xcc', 
+    dsApiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJvMzU0ZDUyZjlmZDQzNDMyIiwidHBlIjoxLCJhcCI6bnVsbCwicyI6IjFmMDg4YzY2LWVkZjAtNjc1Ny05MGMxLWZhMWM2YjUyMmZiNSIsImlzcyI6ImRhdGFzcGlrZS5pbyJ9._z0L9KVqZs1qO7P1A7j9Hwa9QpCzFsKcrDBLZpeumL4', 
     shortId: '',
   );
 }
