f7d976f7759d8d1051106f8b1ee7871dd8938df2
Mikhail Tiranov <mikhail.tiranov@mozzartbet.com>
2025-09-05 21:55:22 +0200

    small refactor

--- lib/res/colors/app_colors.dart
+++ lib/res/colors/app_colors.dart
@@ -2,16 +2,29 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Colors.red;
+
+  
   static const Color darkGrey = Color(0xFF737390);
   static const Color darkAccent = Color(0xFF6664E8);
   static const Color secondaryDarkAccent = Color(0xFF5A58DF);
 
   static const Color secondaryLightBackground = Color(0xFFDCE2FA);
   static const Color lightBackground = Color(0xFFF0F0FD);
-  static const Color white = Color(0xFFFFFFFF);
+  
 
   static const Color alternativeDivider = Color(0xFFD3D8F8);
   static const Color dividerSecondary = Color(0xFFF6F8FE);
 
-  static const Color red = Colors.red;
+
+  //   static const Color darkIndigo = Color(0xFF737390);        // серо-фиолетовый
+// static const Color royalPurple = Color(0xFF6664E8);       // насыщенный фиолетовый
+// static const Color deepViolet = Color(0xFF5A58DF);        // чуть темнее акцент
+
+// static const Color softLavender = Color(0xFFDCE2FA);      // нежно-лавандовый фон
+// static const Color mistyLilac = Color(0xFFF0F0FD);        // почти белый с лавандовым оттенком
+
+// static const Color palePeriwinkle = Color(0xFFD3D8F8);    // светлый разделитель
+// static const Color snowyLilac = Color(0xFFF6F8FE);        // очень светлый фон-разделитель
 }
