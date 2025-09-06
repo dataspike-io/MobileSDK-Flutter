fdb940c0f4e42fca1374b70a3050b63ee672ad48
Mikhail Tiranov <mikhail.tiranov@mozzartbet.com>
2025-09-06 11:47:58 +0200

    add fix in coordinator

--- lib/main/coordinator/coordinator.dart
+++ lib/main/coordinator/coordinator.dart
@@ -5,13 +5,20 @@ import 'package:dataspikemobilesdk/view/screens/camera/camera_avatar_screen.dart
 import 'package:dataspikemobilesdk/view/screens/camera/camera_document_screen.dart';
 import 'package:dataspikemobilesdk/view/screens/personal_data_screen/personal_data_screen.dart';
 
+enum DataspikeStep {
+  onboarding,
+  personalData,
+  selfieCamera,
+  documentCamera,
+}
+
 class DataspikeCoordinator {
   static void startFlow(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) => DataspikeScreen(
           onSuccess: (context) {
-            _showOnboarding(context);
+            showNextStep(context, DataspikeStep.onboarding);
           },
           onFail: (context) {
             // Handle failure
@@ -21,35 +28,48 @@ class DataspikeCoordinator {
     );
   }
 
-  static void _showOnboarding(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) => OnboardingScreen(
-          onStart: () {
-            showPersonalData(context);
-          },
-        ),
-      ),
-    );
+  static void showNextStep(BuildContext context, DataspikeStep step) {
+    switch (step) {
+      case DataspikeStep.onboarding:
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => OnboardingScreen(
+              onStart: () => showNextStep(context, DataspikeStep.personalData),
+            ),
+          ),
+        );
+        break;
+      case DataspikeStep.personalData:
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (_) => const PersonalDataScreen()),
+        );
+        break;
+      case DataspikeStep.selfieCamera:
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => LiveAvatarCamera(onCropped: (value) {}),
+          ),
+        );
+        break;
+      case DataspikeStep.documentCamera:
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => LiveCropCamera(onCropped: (value) {}),
+          ),
+        );
+        break;
+    }
   }
 
-  static void showSelfieCamera(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) => LiveAvatarCamera(onCropped: (value) {}),
-      ),
-    );
-  }
+  static void _showOnboarding(BuildContext context) =>
+      showNextStep(context, DataspikeStep.onboarding);
 
-  static void showDocumentCamera(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => LiveCropCamera(onCropped: (value) {})),
-    );
-  }
+  static void showSelfieCamera(BuildContext context) =>
+      showNextStep(context, DataspikeStep.selfieCamera);
 
-  static void showPersonalData(BuildContext context) {
-    Navigator.of(
-      context,
-    ).push(MaterialPageRoute(builder: (_) => const PersonalDataScreen()));
-  }
+  static void showDocumentCamera(BuildContext context) =>
+      showNextStep(context, DataspikeStep.documentCamera);
+
+  static void showPersonalData(BuildContext context) =>
+      showNextStep(context, DataspikeStep.personalData);
 }
