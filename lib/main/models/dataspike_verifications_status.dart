277c7fc7f51947dfcbe1f93b64b39b689fc20ef5
Mikhail Tiranov <mikhail.tiranov@mozzartbet.com>
2026-01-25 19:14:46 +0100

    small changes

--- lib/main/models/dataspike_verifications_status.dart
+++ lib/main/models/dataspike_verifications_status.dart
@@ -1,11 +1,3 @@
-// class DataspikeVerificationStatus {
-//   final bool success;
-//   final String? details;
-//   final String? error;
-
-//   DataspikeVerificationStatus({required this.success, this.details, this.error});
-// }
-
 enum DataspikeVerificationStatus {
   verificationCompleted,
   verificationFailed,
